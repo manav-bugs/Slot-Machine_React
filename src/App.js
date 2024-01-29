ec8c96c969fbf684aa003063de9bbe3455cae9f5
manav-bugs <manavmodi1102@gmail.com>
2024-01-29 18:41:57 +0530

    Intial

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const  App = () =>{
+  return <>
+    <h1 className='heading_style'>
+       🎰 Welcome to <span style={{fontWeight:"bold"}}>
+         Slot machine game</span> 🎰
+        </h1>,
+
+
+  </>;
+}
+
+export default App
